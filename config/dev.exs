c4d5f37e0e6f0a4f5c2b64d75b245ec6c7677382
Allan Egidio <allan.egidio@outlook.com>
2023-05-08 18:48:17 -0300

    Add npm

--- config/dev.exs
+++ config/dev.exs
@@ -8,7 +8,8 @@ config :ex_ads, ExAds.Repo,
   database: "ex_ads_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 10,
+  port: 5430
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
